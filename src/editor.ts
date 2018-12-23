bda9efac801a774f0e0b9a193c5b9d35f5e9a8cf
yuvipanda <yuvipanda@gmail.com>
2018-12-22 18:38:09 -0800

    Split extension into multiple files

---
+++ src/editor.ts
@@ -0,0 +1,87 @@
+// Don't try to find typescript definitions for jsoneditor
+// @types/jsoneditor exists but is too old, and won't let us
+// use some properties we *do* want to use. Meh.
+//@ts-ignore
+import JSONEditor from "jsoneditor/dist/jsoneditor.js";
+import 'jsoneditor/dist/jsoneditor.css';
+import 'jsoneditor/dist/img/jsoneditor-icons.svg';
+
+import {
+  JSONObject
+} from '@phosphor/coreutils';
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
+  INotebookTracker
+} from '@jupyterlab/notebook';
+
+class MetadataEditorWidget extends Widget {
+  readonly containerDiv: HTMLDivElement
+  readonly editorDiv: HTMLDivElement;
+  readonly notebookTracker: INotebookTracker;
+
+  readonly editor: JSONEditor;
+  constructor(notebookTracker: INotebookTracker) {
+    super();
+
+    this.notebookTracker = notebookTracker;
+
+    this.id = 'notebook-metadata-editor';
+    this.title.label = 'Notebook Metadata Editor';
+    this.title.closable = true;
+
+    this.addClass('jp-MetadataEditorWidget')
+
+    this.containerDiv = document.createElement('div');
+    this.node.appendChild(this.containerDiv);
+    this.editorDiv = document.createElement('div');
+    this.containerDiv.appendChild(this.editorDiv);
+
+    this.editor = new JSONEditor(this.editorDiv, {
+      name: 'Notebook Metadata',
+      mainMenuBar: false,
+      navigationBar: false,
+      enableSort: false,
+      enableTransform: false,
+      onChangeJSON: (json: JSONObject) => {
+        this.setCurrentNotebookMetadata(json);
+      }
+    }, {});
+
+    this.notebookTracker.currentChanged.connect(() => {
+      let curNotebookWidget = this.notebookTracker.currentWidget;
+      if (curNotebookWidget.isAttached) {
+        let metadata = curNotebookWidget.content.model.metadata;
+        metadata.changed.connect(() => {
+          console.log(metadata.toJSON());
+          if (metadata.toJSON() != this.editor.get()) {
+            this.editor.setText(JSON.stringify(metadata.toJSON()))
+          }
+        })
+
+        // Uh, we can't pass the object
+        this.editor.setText(JSON.stringify(metadata));
+      }
+      return true;
+    });
+  }
+
+  setCurrentNotebookMetadata(metadata: JSONObject) {
+      let curNotebookWidget = this.notebookTracker.currentWidget;
+      if (curNotebookWidget != null && curNotebookWidget.isAttached) {
+        let notebookModel = curNotebookWidget.content.model;
+        // No way to atomically set metadata?
+        notebookModel.metadata.clear();
+        for (let key in metadata) {
+          notebookModel.metadata.set(key, metadata[key]);
+        }
+        notebookModel.dirty = true;
+      }
+
+  }
+}
+
+export default MetadataEditorWidget;
--- src/index.ts
+++ src/index.ts
@@ -1,129 +1,16 @@
-import {
-  IDisposable, DisposableDelegate
-} from '@phosphor/disposable';
+import { INotebookTracker } from '@jupyterlab/notebook';
 
-import {
-  ToolbarButton
-} from '@jupyterlab/apputils';
+import { JupyterLab, JupyterLabPlugin, ILayoutRestorer } from '@jupyterlab/application';
 
-import {
-  DocumentRegistry
-} from '@jupyterlab/docregistry';
+import { ICommandPalette, InstanceTracker } from '@jupyterlab/apputils';
 
-import {
-  NotebookPanel, INotebookModel, INotebookTracker
-} from '@jupyterlab/notebook';
+import { JSONExt } from '@phosphor/coreutils';
 
-import {
-  JupyterLab, JupyterLabPlugin, ILayoutRestorer
-} from '@jupyterlab/application';
+import { Widget } from '@phosphor/widgets';
 
-import {
-  ICommandPalette, InstanceTracker
-} from '@jupyterlab/apputils';
-
-import {
-  JSONExt, JSONObject
-} from '@phosphor/coreutils';
-
-import {
-  Widget
-} from '@phosphor/widgets';
-
-// Don't try to find typescript definitions for jsoneditor
-// @types/jsoneditor exists but is too old, and won't let us
-// use some properties we *do* want to use. Meh.
-//@ts-ignore
-import JSONEditor from "jsoneditor/dist/jsoneditor.js";
-
+import MetadataEditorWidget from "./editor";
+import EditMetadataButton from "./toolbar";
 import '../style/index.css';
-import 'jsoneditor/dist/jsoneditor.css';
-import 'jsoneditor/dist/img/jsoneditor-icons.svg';
-
-class MetadataEditorWidget extends Widget {
-  readonly containerDiv: HTMLDivElement
-  readonly editorDiv: HTMLDivElement;
-  readonly notebookTracker: INotebookTracker;
-
-  readonly editor: JSONEditor;
-  constructor(notebookTracker: INotebookTracker) {
-    super();
-
-    this.notebookTracker = notebookTracker;
-
-    this.id = 'notebook-metadata-editor';
-    this.title.label = 'Notebook Metadata Editor';
-    this.title.closable = true;
-
-    this.addClass('jp-MetadataEditorWidget')
-
-    this.containerDiv = document.createElement('div');
-    this.node.appendChild(this.containerDiv);
-    this.editorDiv = document.createElement('div');
-    this.containerDiv.appendChild(this.editorDiv);
-
-    this.editor = new JSONEditor(this.editorDiv, {
-      name: 'Notebook Metadata',
-      mainMenuBar: false,
-      navigationBar: false,
-      enableSort: false,
-      enableTransform: false,
-      onChangeJSON: (json: JSONObject) => {
-        this.setCurrentNotebookMetadata(json);
-      }
-    }, {});
-
-    this.notebookTracker.currentChanged.connect(() => {
-      let curNotebookWidget = this.notebookTracker.currentWidget;
-      if (curNotebookWidget.isAttached) {
-        let metadata = curNotebookWidget.content.model.metadata;
-        metadata.changed.connect(() => {
-          console.log(metadata.toJSON());
-          if (metadata.toJSON() != this.editor.get()) {
-            this.editor.setText(JSON.stringify(metadata.toJSON()))
-          }
-        })
-
-        // Uh, we can't pass the object
-        this.editor.setText(JSON.stringify(metadata));
-      }
-      return true;
-    });
-  }
-
-  setCurrentNotebookMetadata(metadata: JSONObject) {
-      let curNotebookWidget = this.notebookTracker.currentWidget;
-      if (curNotebookWidget != null && curNotebookWidget.isAttached) {
-        let notebookModel = curNotebookWidget.content.model;
-        // No way to atomically set metadata?
-        notebookModel.metadata.clear();
-        for (let key in metadata) {
-          notebookModel.metadata.set(key, metadata[key]);
-        }
-        notebookModel.dirty = true;
-      }
-
-  }
-}
-
-class EditMetadataButton implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
-    let callback = () => {
-      console.log('hi')
-    };
-    let button = new ToolbarButton({
-      className: 'jp-nbmetadata-edit-metadata',
-      iconClassName: 'fa fa-fast-forward',
-      onClick: callback,
-      tooltip: 'Edit Notebook Metadata'
-    });
-
-    panel.toolbar.addItem('edit-metadata', button);
-    return new DisposableDelegate(() => {
-      button.dispose();
-    });
-  }
-}
 
 function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRestorer, notebookTracker: INotebookTracker) {
     let widget: MetadataEditorWidget;
@@ -145,7 +32,8 @@ function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRe
 
         widget.update();
         app.shell.activateById(widget.id);
-      }
+      },
+
     })
 
     palette.addItem({command, category: 'Notebook'});
